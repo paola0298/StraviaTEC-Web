daa4e337c383799416974111e372a0587a19585b
Paola Villegas Chacón <37082461+paola0298@users.noreply.github.com>
2020-11-01 10:36:07 -0600

    Actualizado modelo

--- StraviaTec-Web/ClientApp/src/app/models/user.ts
+++ StraviaTec-Web/ClientApp/src/app/models/user.ts
@@ -1,12 +1,12 @@
 export class Usuario {
-    User: string;
-    Password: string;
-    Nombre: string;
-    Apellido1: string;
-    Apellido2: string;
-    Fecha_nacimiento: string;
-    Nacionalidad: string;
-    Foto: string;
+    user: string;
+    password: string;
+    nombre: string;
+    apellido1: string;
+    apellido2: string;
+    fecha_nacimiento: string;
+    nacionalidad: string;
+    foto: string;
 
     constructor(
         User: string,
@@ -18,13 +18,13 @@ export class Usuario {
         Nacionalidad: string,
         Foto: string
     ){
-        this.User = User;
-        this.Password = Password;
-        this.Nombre = Nombre;
-        this.Apellido1 = Apellido1;
-        this.Apellido2 = Apellido2;
-        this.Fecha_nacimiento = Fecha_nacimiento;
-        this.Nacionalidad = Nacionalidad;
-        this.Foto = Foto;
+        this.user = User;
+        this.password = Password;
+        this.nombre = Nombre;
+        this.apellido1 = Apellido1;
+        this.apellido2 = Apellido2;
+        this.fecha_nacimiento = Fecha_nacimiento;
+        this.nacionalidad = Nacionalidad;
+        this.foto = Foto;
     }
 }
