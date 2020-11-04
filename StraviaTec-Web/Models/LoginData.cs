e42a670b33fa5cba8ad444793d2bb8a160610acf
Paola Villegas Chacón <37082461+paola0298@users.noreply.github.com>
2020-11-04 13:09:42 -0600

    Corregido atributo

--- StraviaTec-Web/Models/LoginData.cs
+++ StraviaTec-Web/Models/LoginData.cs
@@ -3,7 +3,7 @@ namespace StraviaTec_Web.Models
     public class LoginData
     {
         public string User { get; set; }
-        public string Pasword { get; set; }
+        public string Password { get; set; }
         
     }
 }
