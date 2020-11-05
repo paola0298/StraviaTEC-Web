721bfd5c4ff596d4bb1c2f38a5c948f99f6b45d0
Paola Villegas Chacón <37082461+paola0298@users.noreply.github.com>
2020-11-04 21:56:44 -0600

    Actualizados modelos

--- StraviaTec-Web/Models/InscripcionEvento.cs
+++ StraviaTec-Web/Models/InscripcionEvento.cs
@@ -8,6 +8,7 @@ namespace StraviaTec_Web.Models
         public int Id { get; set; }
         public int IdEvento { get; set; }
         public string IdUsuario { get; set; }
+        public string Estado { get; set; }
 
         public virtual Evento IdEventoNavigation { get; set; }
         public virtual Usuario IdUsuarioNavigation { get; set; }
