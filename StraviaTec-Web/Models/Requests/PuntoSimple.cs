108fa0285e99bda45d81ca4f4b8ff49075583cf6
Marlon Vega <mvegach2013@gmail.com>
2020-11-11 22:12:51 -0600

    Modelo para enviar información básica de puntos

---
+++ StraviaTec-Web/Models/Requests/PuntoSimple.cs
@@ -0,0 +1,10 @@
+namespace StraviaTec_Web.Models.Requests
+{
+    public class PuntoSimple
+    {
+        public int Segmento { get; set; }
+        public int Orden { get; set; }
+        public decimal Lat { get; set; }
+        public decimal Lng { get; set; }
+    }
+}
