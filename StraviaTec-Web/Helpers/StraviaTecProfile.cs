112b0bc6b9135a23a524b36f8878ae317b37913a
Marlon Vega <mvegach2013@gmail.com>
2020-11-11 22:10:25 -0600

    Clase para mapear entidades a DTOs

---
+++ StraviaTec-Web/Helpers/StraviaTecProfile.cs
@@ -0,0 +1,15 @@
+using AutoMapper;
+using StraviaTec_Web.Models;
+using StraviaTec_Web.Models.Dtos;
+
+namespace StraviaTec_Web.Helpers
+{
+    public class StraviaTecProfile: Profile
+    {
+        public StraviaTecProfile()
+        {
+            //Insertar mapeo de datos de una clase a otra
+            CreateMap<Carrera, CarreraDto>();
+        }
+    }
+}
