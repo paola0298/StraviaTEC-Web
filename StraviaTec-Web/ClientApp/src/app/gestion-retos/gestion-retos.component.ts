2bb4a4aa1312af33f5ad15827cd0647af7aed1c4
Paola Villegas Chacón <37082461+paola0298@users.noreply.github.com>
2020-11-13 10:32:49 -0600

    Creado componente de gestion de retos

---
+++ StraviaTec-Web/ClientApp/src/app/gestion-retos/gestion-retos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gestion-retos',
+  templateUrl: './gestion-retos.component.html',
+  styleUrls: ['./gestion-retos.component.css']
+})
+export class GestionRetosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
