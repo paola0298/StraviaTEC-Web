135f8bd34b27ba224a9606bed7f285cf049c5da3
Paola Villegas Chacón <37082461+paola0298@users.noreply.github.com>
2020-11-13 12:23:57 -0600

    Creada la base del controlador de grupos

---
+++ StraviaTec-Web/Controllers/GruposController.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StraviaTec_Web.Models;
+
+namespace Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GruposController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public GruposController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Grupos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Grupo>>> GetGrupo()
+        {
+            return await _context.Grupo.ToListAsync();
+        }
+
+        // GET: api/Grupos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Grupo>> GetGrupo(int id)
+        {
+            var grupo = await _context.Grupo.FindAsync(id);
+
+            if (grupo == null)
+            {
+                return NotFound();
+            }
+
+            return grupo;
+        }
+
+        // PUT: api/Grupos/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutGrupo(int id, Grupo grupo)
+        {
+            if (id != grupo.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(grupo).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GrupoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Grupos
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Grupo>> PostGrupo(Grupo grupo)
+        {
+            _context.Grupo.Add(grupo);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetGrupo", new { id = grupo.Id }, grupo);
+        }
+
+        // DELETE: api/Grupos/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Grupo>> DeleteGrupo(int id)
+        {
+            var grupo = await _context.Grupo.FindAsync(id);
+            if (grupo == null)
+            {
+                return NotFound();
+            }
+
+            _context.Grupo.Remove(grupo);
+            await _context.SaveChangesAsync();
+
+            return grupo;
+        }
+
+        private bool GrupoExists(int id)
+        {
+            return _context.Grupo.Any(e => e.Id == id);
+        }
+    }
+}
