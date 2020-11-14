cf70c4c9dd67aa59fa27553bd2b2bffdc463970d
Paola Villegas Chacón <37082461+paola0298@users.noreply.github.com>
2020-11-14 10:31:34 -0600

    Creado componente para asociarse a un grupo

---
+++ StraviaTec-Web/ClientApp/src/app/asociarse-grupo/asociarse-grupo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-asociarse-grupo',
+  templateUrl: './asociarse-grupo.component.html',
+  styleUrls: ['./asociarse-grupo.component.css']
+})
+export class AsociarseGrupoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
