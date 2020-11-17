8e9f3fb07ac625856eb16f56d93741bea35aeebd
Marlon Vega <mvegach2013@gmail.com>
2020-11-17 17:00:22 -0600

    Creada pantalla para cargar actividad

--- StraviaTec-Web/ClientApp/src/app/upload-activity/upload-activity.component.ts
+++ StraviaTec-Web/ClientApp/src/app/upload-activity/upload-activity.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-upload-activity',
@@ -7,9 +8,147 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadActivityComponent implements OnInit {
 
-  constructor() { }
+  fecha: HTMLInputElement;
+  hora: HTMLInputElement;
+  kilometraje: HTMLInputElement;
+  duracion: HTMLInputElement;
+  recorrido: HTMLInputElement;
+  recorridoData: string;
+  tipoActividad: HTMLSelectElement;
+  tipoEvento: HTMLSelectElement;
+  carreras: HTMLSelectElement;
+  retos: HTMLSelectElement;
+  esEvento: boolean = false;
+  idUsuario: string;
+
+  constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
+    this.idUsuario = window.localStorage.getItem('userId');
+    console.log(this.idUsuario);
+
+    this.fecha = document.getElementById('fecha') as HTMLInputElement;
+    this.hora = document.getElementById('hora') as HTMLInputElement;
+    this.kilometraje = document.getElementById('kilometraje') as HTMLInputElement;
+    this.duracion = document.getElementById('duracion') as HTMLInputElement;
+    this.recorrido = document.getElementById('recorrido') as HTMLInputElement;
+    this.tipoActividad = document.getElementById('tipo-actividad') as HTMLSelectElement;
+    this.tipoEvento = document.getElementById('evento') as HTMLSelectElement;
+    this.carreras = document.getElementById('carreras') as HTMLSelectElement;
+    this.retos = document.getElementById('retos') as HTMLSelectElement;
+
+    let today = new Date();
+    this.fecha.valueAsDate = today;
+    let hours = today.getHours();
+    let mins = today.getMinutes();
+    this.hora.value = (hours < 10 ? '0' + hours : hours) + ":" + (mins < 10 ? '0' + mins : mins);
+
+    this.loadTiposActividad();
   }
-  
+
+  loadTiposActividad() {
+    this.apiService.get(`http://localhost:${this.apiService.PORT}/api/InfoEvento/tipos`)
+      .subscribe((data: any[]) => {
+        for (let i = 0; i < data.length; i++) {
+          this.tipoActividad.appendChild(new Option(data[i].nombre, data[i].id));
+        }
+      });
+  }
+
+  recorridoChanged(event: Event) {
+    let self = this;
+    let file = this.recorrido.files[0];
+    let reader = new FileReader();
+    reader.onload = (data) => {
+      console.log("File read");
+      self.recorridoData = data.target.result.toString();
+    };
+    reader.readAsDataURL(file);
+  }
+
+  addActividad(event: Event) {
+    console.log('Saving...')
+
+    var data = {
+      Fecha: this.fecha.value,
+      Hora: this.hora.value,
+      kilometraje: this.kilometraje.value,
+      Duracion: this.duracion.value,
+      Recorrido: this.recorridoData,
+      TipoActividad: this.tipoActividad.value,
+      EsEvento: this.esEvento.toString()
+    };
+
+    if (!this.checkData()) {
+      console.log(data);
+      this.showError("Debes completar todos los campos");
+      return;
+    }
+
+    if (this.esEvento) {
+      data['TipoEvento'] = this.tipoEvento.value;
+
+      if (this.tipoEvento.value === "reto") {
+        if (this.retos.value == "")
+          this.showError("Debes seleccionar un reto");
+        data['Reto'] = this.retos.value;
+      } else if (this.tipoEvento.value === "carrera") {
+        if (this.carreras.value == "")
+          this.showError("Debes seleccionar una carrera");
+        data['Carrera'] = this.carreras.value;
+      } else {
+        //Show error
+        this.showError("Debes seleccionar un tipo de evento");
+      }
+    }
+    
+    console.log(data);
+
+    //TODO: enviar información al backend
+    // this.apiService.post(`http://localhost:${this.apiService.PORT}/api/Actividad`, data);
+  }
+
+  checkData() {
+    return (
+      this.fecha.value != "" && 
+      this.hora.value != "" &&
+      this.kilometraje.value != "" &&
+      this.duracion.value != "" &&
+      this.recorrido.value != "" &&
+      this.tipoActividad.value != ""
+    );
+  }
+
+  showError(msg: string) {
+    let lbl = document.getElementById('error-msg');
+    lbl.innerText = msg;
+
+    // @ts-ignore
+    jQuery('#errorModal').modal();
+  }
+
+  esEventoChanged(event: Event) {
+    this.esEvento = JSON.parse((event.target as HTMLInputElement).value);
+    
+    let eventoContainer = document.getElementById('evento-select');
+    eventoContainer.style.setProperty('display', this.esEvento ? 'flex': 'none');
+  }
+
+  onEventoTypeChange(event: Event) {
+    var type = (event.target as HTMLSelectElement).value
+
+    let carreras = document.getElementById('carrera-select');
+    let retos = document.getElementById('reto-select');
+
+    if (type === "carrera") {
+      carreras.style.setProperty('display', 'block');
+      retos.style.setProperty('display', 'none');
+    } else if (type === "reto") {
+      carreras.style.setProperty('display', 'none');
+      retos.style.setProperty('display', 'block');
+    } else {
+      carreras.style.setProperty('display', 'none');
+      retos.style.setProperty('display', 'none');
+    }
+  }
 }
