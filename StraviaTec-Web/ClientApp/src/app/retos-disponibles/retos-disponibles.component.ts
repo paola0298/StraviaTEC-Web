455a68a464ab10db78793b9473423bc30a2fe918
Paola Villegas Chacón <37082461+paola0298@users.noreply.github.com>
2020-11-21 08:21:40 -0600

    Update retos-disponibles.component.ts

--- StraviaTec-Web/ClientApp/src/app/retos-disponibles/retos-disponibles.component.ts
+++ StraviaTec-Web/ClientApp/src/app/retos-disponibles/retos-disponibles.component.ts
@@ -12,64 +12,41 @@ import { Patrocinador } from '../models/patrocinador';
 })
 export class RetosDisponiblesComponent implements OnInit {
   // TODO Obtener retos reales esperar a que esté listo backend, descomentar código cuando esto pase
-  // retos: Reto[];
+  retos: Reto[];
+  userId = window.localStorage.getItem('userId');
 
-  patrocinadores = [
-    new Patrocinador(
-      3,
-      'Visa',
-      'Representante Visa',
-      '12345678',
-      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F0%2F04%2FVisa.svg%2F1200px-Visa.svg.png&f=1&nofb=1'
-    ),
-  ];
+  // patrocinadores = [
+  //   new Patrocinador(
+  //     3,
+  //     'Visa',
+  //     'Representante Visa',
+  //     '12345678',
+  //     'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F0%2F04%2FVisa.svg%2F1200px-Visa.svg.png&f=1&nofb=1'
+  //   ),
+  // ];
 
-  retos = [
-    new Reto(
-      1,
-      'Reto 1',
-      '20 Diciembre',
-      '31 Diciembre',
-      5000,
-      1,
-      4,
-      this.patrocinadores,
-      [],
-      true
-    ),
-    new Reto(
-      2,
-      'Reto 2',
-      '10 Enero',
-      '20 Enero',
-      2500,
-      2,
-      6,
-      this.patrocinadores,
-      [],
-      true
-    ),
-  ];
+  // retos = [
+  //   new Reto(1,'Reto 1','20 Diciembre','31 Diciembre',5000,1,4,this.patrocinadores,[],true),
+  //   new Reto(2,'Reto 2','10 Enero','20 Enero',2500,2,6,this.patrocinadores,[],true),
+  // ];
 
-  constructor(
-    private utilsService: UtilsService,
-    private apiService: ApiService,
-    private router: Router
-  ) {}
+  constructor(private utilsService: UtilsService, private apiService: ApiService, private router: Router) {}
 
   ngOnInit(): void {
-    // this.loadChallenges();
+    this.loadChallenges();
   }
 
   async loadChallenges() {
-    const userId = window.localStorage.getItem('userId');
-    console.log('current user: ' + userId);
-    const response = this.apiService.get(
-      `http://127.0.0.1:${this.apiService.PORT}/api/Reto/user/${userId}`
+    console.log('current user: ' + this.userId);
+    const response = this.apiService.get(`http://127.0.0.1:${this.apiService.PORT}/api/Reto/user/${this.userId}`
     );
     response.subscribe((value: Reto[]) => {
       console.log(value);
-      // this.retos = value;
+      this.retos = value;
+    }, (error:any) => {
+      console.log(error.statusText);
+      console.log(error.status);
+      console.log(error);
     });
   }
 }
