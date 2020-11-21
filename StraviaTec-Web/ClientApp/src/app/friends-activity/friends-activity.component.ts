9bce41c49f9b936b380afb55128d5bde75288fde
Paola Villegas Chacón <37082461+paola0298@users.noreply.github.com>
2020-11-21 17:15:11 -0600

    Creado componente de actividades de amigos

---
+++ StraviaTec-Web/ClientApp/src/app/friends-activity/friends-activity.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-friends-activity',
+  templateUrl: './friends-activity.component.html',
+  styleUrls: ['./friends-activity.component.css']
+})
+export class FriendsActivityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
